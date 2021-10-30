afe65960de15f87a7c7ce89e5f056f0385f3686f
okatsuo <rafael.goncanna@gmail.com>
2021-10-30 15:46:35 -0300

    feat: fake-message-repository to test subscription

--- src/main/resolver/message-resolver.ts
+++ src/main/resolver/message-resolver.ts
@@ -1,10 +1,20 @@
-import { Query, Resolver } from 'type-graphql'
+import { Arg, Mutation, Query, Resolver } from 'type-graphql'
+import { MessageInputInterface, MessageRepository } from '../../fake-repository'
 import { MessageSchema } from '../schema/message-schema'
 
 @Resolver(MessageSchema)
 export class MessageResolver {
-  @Query(() => MessageSchema)
-  async messages (): Promise<MessageSchema> {
-    return { id: 1, message: 'vamo que vamo!', created_at: new Date(), updated_at: new Date() }
+  private messagesRepository = new MessageRepository()
+
+  @Query(() => [MessageSchema])
+  async messages (): Promise<MessageSchema[]> {
+    return this.messagesRepository.messages()
+  }
+
+  @Mutation(() => MessageSchema)
+  async messageCreate (
+    @Arg('fields', () => MessageInputInterface) fields: MessageInputInterface
+  ): Promise<MessageSchema> {
+    return this.messagesRepository.messageCreate(fields)
   }
 }
